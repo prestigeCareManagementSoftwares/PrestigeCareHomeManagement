d1f92678bb9b623dae1ff3f05b96fec4c68768d5
prestigeCareManagementSoftwares <olivia.smith.staff@gmail.com>
2025-08-22 10:29:36 +0100

    media settings for prod & debug env

--- carehome_project/urls.py
+++ carehome_project/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from carehome_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+# Always serve media (dev + prod)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
