0227b1854cedd0dfc5ff83cdac50642b026e7756
maham904 <mahamkhanlu@gmail.com>
2025-09-14 11:38:30 +0100

    date of joining change

--- core/models.py
+++ core/models.py
@@ -374,7 +374,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
