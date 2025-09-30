fc60b9d5bc12f0c8bb8c6d947b66555f1009c412
maham904 <mahamkhanlu@gmail.com>
2025-09-30 20:26:40 +0100

    reset password

--- carehome_project/settings.py
+++ carehome_project/settings.py
@@ -154,4 +154,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("BREVO_EMAIL")  # e.g. your Brevo account email
 EMAIL_HOST_PASSWORD = os.environ.get("BREVO_SMTP_KEY")  # Brevo SMTP key
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "no-reply@prestigesupportedliving.com")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
